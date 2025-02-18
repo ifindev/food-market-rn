aafdb7cc8fd97f3a1e803b544824fa5a7ac4db35
Muhammad Arifin <muhammad.arifin@192.168.1.22>
2025-02-18 16:07:32 +0700

    feat: setup initial app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:prettier/recommended'],
+  plugins: ['prettier', 'unused-imports'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn', // Optional: Keeps unused variables as a warning
+    'unused-imports/no-unused-imports': 'error', // Removes unused imports
+  },
 };
---
+++ src/app/App.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, useColorScheme, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import UserGreetings from '../components/UserGreetings';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={{...backgroundStyle, ...styles.safeAreaContainer}}>
+      <View style={styles.container}>
+        <UserGreetings />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeAreaContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    height: '100%',
+    padding: 24,
+  },
+  userContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 50,
+  },
+  userGreetingContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 4,
+  },
+  greeting: {
+    color: '#787676',
+    fontSize: 14,
+  },
+  fullName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
