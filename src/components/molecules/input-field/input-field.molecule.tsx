d222591e8a7c316da96624dbea3f6df7dea3ff1d
ifindev <arifin.muhammad2610@gmail.com>
2025-02-19 14:42:23 +0700

    feat: create input-field component

---
+++ src/components/molecules/input-field/input-field.molecule.tsx
@@ -0,0 +1,26 @@
+import Input from '@components/atoms/input/input.atom';
+import Typography from '@components/atoms/typography/typography.atom';
+import React from 'react';
+import {StyleSheet, TextInputProps, View} from 'react-native';
+
+type Props = TextInputProps & {
+  label: string;
+  error?: string;
+};
+
+export default function InputField({label, ...inputProps}: Props) {
+  return (
+    <View style={styles.container}>
+      <Typography.Body16>{label}</Typography.Body16>
+      <Input {...inputProps} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 6,
+  },
+});
