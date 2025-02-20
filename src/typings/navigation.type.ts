eadb0b82399de6abdcd97afa0b7b7ccc31655a3b
ifindev <arifin.muhammad2610@gmail.com>
2025-02-20 17:37:38 +0700

    feat: add route types definitions

--- src/typings/navigation.type.ts
+++ src/typings/navigation.type.ts
@@ -1,11 +1,36 @@
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {FunctionComponent} from 'react';
+import {
+  NativeStackScreenProps,
+  NativeStackNavigationOptions,
+} from '@react-navigation/native-stack';
 
+// #region AUTH ROUTES STACK TYPES
 export type AuthStackParamList = {
   Login: undefined;
   Register: undefined;
   RegisterAddress: undefined;
 };
 
-// Define a type for the navigation prop in each screen
 export type AuthScreenProps<T extends keyof AuthStackParamList> =
   NativeStackScreenProps<AuthStackParamList, T>;
+// #endregion
+
+// #region PROTECTED ROUTES
+export type ProtectedStackParamList = {
+  Home: undefined;
+  FoodDetail: {id: string};
+  Orders: undefined;
+};
+// #endregion
+
+// #region GENERIC APPROUTE TYPE
+export type AppRoute<T = {}, StackParamList = {}> = {
+  name: keyof StackParamList;
+  component: FunctionComponent<T>;
+  options?: NativeStackNavigationOptions;
+};
+
+export type AppAuthRoute = AppRoute<{}, AuthStackParamList>;
+
+export type AppProtectedRoute = AppRoute<{}, ProtectedStackParamList>;
+// #endregion
