0c1e8e1e3d74564352631fbcd8f05e3fd77ec216
ifindev <arifin.muhammad2610@gmail.com>
2025-02-21 22:22:27 +0700

    feat: update modules grouping

--- src/app/navigation/auth.navigation.tsx
+++ src/app/navigation/auth.navigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import loginRoute from '@modules/login/login.route';
-import registerAddressRoute from '@modules/register-address/register-address.route';
-import registerRoute from '@modules/register/register.route';
+import loginRoute from '@modules/auth/login/login.route';
+import registerAddressRoute from '@modules/auth/register-address/register-address.route';
+import registerRoute from '@modules/auth/register/register.route';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {AppAuthRoute, AuthStackParamList} from '@typings/navigation.type';
 
