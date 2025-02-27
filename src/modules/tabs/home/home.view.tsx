a5510c3c0b73440f24e77cd4b6648dc619c67d25
ifindev <arifin.muhammad2610@gmail.com>
2025-02-27 17:39:34 +0700

    feat: add food-list component to home view

--- src/modules/tabs/home/home.view.tsx
+++ src/modules/tabs/home/home.view.tsx
@@ -3,32 +3,44 @@ import {Alert, StyleSheet, View} from 'react-native';
 import Header from '@components/molecules/header/header.molecule';
 import FeaturedCarousel from '@components/organisms/featured-carousel/featured-carousel.organism';
 import Tab from '@components/molecules/tab/tab.molecule';
+import FoodList from '@components/organisms/food-list/food-list.organism';
 
 export default function HomeView() {
-  const featured = [
+  const menu = [
     {
       id: '1',
       name: 'Cherry Healthy',
       rating: 4.5,
       image: require('../../../assets/cherry-healthy.png'),
+      price: 'IDR 289.000',
     },
     {
       id: '2',
       name: 'Burger Tamago',
       rating: 4.7,
       image: require('../../../assets/burger.png'),
+      price: 'IDR 289.000',
     },
     {
       id: '3',
       name: 'Karee Special',
       rating: 4.3,
       image: require('../../../assets/kari.png'),
+      price: 'IDR 289.000',
     },
     {
       id: '4',
       name: 'Es Tongtong',
       rating: 4.9,
       image: require('../../../assets/es-tongtong.png'),
+      price: 'IDR 289.000',
+    },
+    {
+      id: '5',
+      name: 'Salad',
+      rating: 4.9,
+      image: require('../../../assets/salad.png'),
+      price: 'IDR 289.000',
     },
   ];
 
@@ -43,11 +55,15 @@ export default function HomeView() {
         profileImage="https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=150&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <FeaturedCarousel
-        data={featured}
+        data={menu}
         onClick={item => Alert.alert(`Selected ${item.name}`)}
       />
-      <View style={styles.menuList}>
+      <View style={styles.tabContainer}>
         <Tab items={tabs} selected={selectedTab} onSelect={setSelectedTab} />
+        <FoodList
+          foods={menu}
+          onClick={item => Alert.alert(`Selected ${item.name}`)}
+        />
       </View>
     </View>
   );
@@ -60,7 +76,7 @@ const styles = StyleSheet.create({
     gap: 24,
     backgroundColor: '#FAFAFC',
   },
-  menuList: {
+  tabContainer: {
     flex: 1,
     paddingTop: 16,
     backgroundColor: 'white',
