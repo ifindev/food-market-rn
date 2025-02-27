b8ad6a15c9ccdedc07b364d04d9d36b8a24acf43
ifindev <arifin.muhammad2610@gmail.com>
2025-02-27 15:54:28 +0700

    feat: add featured-carousel component

---
+++ src/components/organisms/featured-carousel.organism.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/no-unstable-nested-components */
+import React from 'react';
+import FeaturedCard, {
+  FeaturedCardProps,
+} from '@components/molecules/featured-card/featured-card.molecule';
+import {FlatList, Pressable, StyleSheet, View} from 'react-native';
+
+type CarouselItem = FeaturedCardProps & {
+  id: string;
+};
+
+type Props = {
+  data: CarouselItem[];
+  onClick: (item: CarouselItem) => void;
+};
+
+export default function FeaturedCarousel({data, onClick}: Props) {
+  return (
+    <FlatList
+      data={data}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={item => item.name}
+      renderItem={({item}) => (
+        <Pressable onPress={() => onClick(item)}>
+          <FeaturedCard
+            name={item.name}
+            image={item.image}
+            rating={item.rating}
+          />
+        </Pressable>
+      )}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  separator: {
+    width: 24,
+  },
+});
