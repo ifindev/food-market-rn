119d1e6751d57c551ddc01d5407c1bca0d18de9b
ifindev <arifin.muhammad2610@gmail.com>
2025-02-27 16:50:27 +0700

    feat: add View container to limit carousel height

--- src/components/organisms/featured-carousel.organism.tsx
+++ src/components/organisms/featured-carousel.organism.tsx
@@ -16,23 +16,25 @@ type Props = {
 
 export default function FeaturedCarousel({data, onClick}: Props) {
   return (
-    <FlatList
-      contentContainerStyle={styles.container}
-      data={data}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={item => item.name}
-      renderItem={({item}) => (
-        <Pressable onPress={() => onClick(item)}>
-          <FeaturedCard
-            name={item.name}
-            image={item.image}
-            rating={item.rating}
-          />
-        </Pressable>
-      )}
-      ItemSeparatorComponent={() => <View style={styles.separator} />}
-    />
+    <View>
+      <FlatList
+        contentContainerStyle={styles.container}
+        data={data}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={item => item.name}
+        renderItem={({item}) => (
+          <Pressable onPress={() => onClick(item)}>
+            <FeaturedCard
+              name={item.name}
+              image={item.image}
+              rating={item.rating}
+            />
+          </Pressable>
+        )}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
+    </View>
   );
 }
 
